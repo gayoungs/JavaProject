0eae2c0b3cf113c9c5b687723ed8a214517629cd
sol0916 <yj0916lee00@gmail.com>
2023-05-27 17:12:49 +0900

    sol 0916 commit

--- src/ramgee/project/db/DBProperties.java
+++ src/ramgee/project/db/DBProperties.java
@@ -1,5 +1,9 @@
 package ramgee.project.db;
 
 public class DBProperties {
+	
+	public static final String URL = "jdbc:oracle:thin:@192.168.219.191:1521:xe";
+	public static final String UID = "RG";
+	public static final String UPW = "RG";
 
 }
--- src/ramgee/project/vo/OrderVO.java
+++ src/ramgee/project/vo/OrderVO.java
@@ -1,5 +1,29 @@
 package ramgee.project.vo;
 
+import java.util.*;
+
 public class OrderVO {
+	
+	private int order_no; //주문번호
+	//private String payment_id; //결제아이디
+	private Date order_date; //주문일자
+	private int total; //총금액
+	private String to_hall; //포장여부
+
+	//기본생성자
+	public OrderVO() {
+	}
+		
+	//생성자
+	public OrderVO(int order_no, Date order_date, int total, String to_hall) {
+		super();
+		this.order_no = order_no;
+		this.order_date = order_date;
+		this.total = total;
+		this.to_hall = to_hall;
+	}
+	
+	
+	
 
 }
