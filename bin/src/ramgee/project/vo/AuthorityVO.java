fe7f176d1a95b454f9fdb55a67f84fb47890d15c
syshin0116 <syshin0116@gmail.com>
2023-05-31 12:17:48 +0900

    ramgee

---
+++ bin/src/ramgee/project/vo/AuthorityVO.java
@@ -0,0 +1,35 @@
+package ramgee.project.vo;
+
+public class AuthorityVO {
+    private int authority_no;
+    private String authorityName;
+
+    @Override
+    public String toString() {
+        return "{" +
+                "authority_no=" + authority_no +
+                ", authorityName='" + authorityName + '\'' +
+                '}';
+    }
+
+    public AuthorityVO(int authority_no, String authorityName) {
+        this.authority_no = authority_no;
+        this.authorityName = authorityName;
+    }
+
+    public int getAuthority_no() {
+        return authority_no;
+    }
+
+    public void setAuthority_no(int authority_no) {
+        this.authority_no = authority_no;
+    }
+
+    public String getAuthorityName() {
+        return authorityName;
+    }
+
+    public void setAuthorityName(String authorityName) {
+        this.authorityName = authorityName;
+    }
+}
--- src/ramgee/project/dao/AdminMain.java
+++
@@ -1,5 +0,0 @@
-package ramgee.project.dao;
-
-public class AdminMain {
-
-}
